3fb05dfc8dc9d6ee880eba6739b50f74d0b4c336
Stephen Snow <spsnow99@hotmail.com>
2022-04-21 13:12:56 -0700

    simple js file to test

---
+++ src/service/math.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+import * as math from './math'
+
+describe('Unit Test refundsService', () => {
+    const {
+        add,
+        // futureNotUsed,
+        // getTheAnswer,
+    } = math
+  
+    before(() => {
+      // check if the import worked correctly
+      expect(add, 'add').to.be.a('function')
+    })
+  
+    context('A. add', () => {
+      it('handles no inputs', () => {
+        expect(add()).to.eq(0)
+      })
+  
+      it('handles two numbers', () => {
+        expect(add(5, 7)).to.eq(12)
+      })
+  
+  
+      it('handles two strings', () => {
+        expect(add('he','llo')).to.eq('hello')
+      })
+  
+    })
+  
+  })

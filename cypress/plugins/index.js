dc204abd7a76ac3f95088f224e52d5b301e3cc25
Stephen Snow <spsnow99@hotmail.com>
2022-04-21 10:34:02 -0700

    updating cypress plugins and support

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,8 +15,14 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+
+const browserify = require('@cypress/browserify-preprocessor')
+
+module.exports = (on) => {
+  on('task', require('cypress-istanbul/task'))
+
+  // tell Cypress to use .babelrc when bundling spec code
+  const options = browserify.defaultOptions
+  options.browserifyOptions.transform[1][1].babelrc = true
+  on('file:preprocessor', browserify(options))
 }

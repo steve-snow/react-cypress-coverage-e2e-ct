b950344b32adf7b394069b4dd284b67fdbbf50e8
Stephen Snow <spsnow99@hotmail.com>
2022-04-22 09:56:49 -0700

    setting up end-2-end test

---
+++ cypress/integration/tests/cra.spec.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+describe("Visit", () => {
+    it("goes to root page and has content", () => {
+        // cy.visit('http://localhost:3000')
+        cy.visit('/')
+        cy.get('.App-link').contains('Learn React')
+    });
+});

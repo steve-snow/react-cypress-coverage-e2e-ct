fe5f3613939e017c5fb482ad7c338bb885388c16
Stephen Snow <spsnow99@hotmail.com>
2022-04-26 10:25:45 -0700

    only coverage is cypress folder

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -25,4 +25,8 @@ module.exports = (on, config) => {
     require('@cypress/react/plugins/react-scripts')(on, config)
   }
 
+  require('@cypress/code-coverage/task')(on, config)
+  on('file:preprocessor', require('@cypress/code-coverage/use-browserify-istanbul'))
+  
+  return config;
 }
